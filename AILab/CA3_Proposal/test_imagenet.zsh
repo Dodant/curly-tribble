55edf461f0a40fde1e54008628cdb14c1ac65cf1
Dodanto <ohho0506@gmail.com>
2021-12-19 13:13:29 +0900

    flush

--- AILab/CA3_Proposal/test_imagenet.zsh
+++
@@ -1,5 +0,0 @@
-echo 'Start'
-python kd_train_imagenet.py -s 18 -t 2 -alpha 0.5 -epochs 25 -batch 256
-python kd_train_imagenet.py -s 34 -t 2 -alpha 0.5 -epochs 25 -batch 256
-python kd_train_imagenet.py -s 50 -t 2 -alpha 0.5 -epochs 25 -batch 128
-echo 'Mischief Managed'
