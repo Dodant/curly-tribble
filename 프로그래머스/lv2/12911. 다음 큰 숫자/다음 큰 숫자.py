72a178b11730e3286da1b822384a725d2d0920a9
Junggyun Oh <ohho0506@gmail.com>
2023-03-03 11:42:35 +0900

    [level 2] Title: 다음 큰 숫자, Time: 0.01 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv2/12911. 다음 큰 숫자/다음 큰 숫자.py
+++ 프로그래머스/lv2/12911. 다음 큰 숫자/다음 큰 숫자.py
@@ -2,7 +2,7 @@ def solution(n):
     answer = n
     while True:
         answer += 1
-        if bin(n).replace('0b','').count('1') == bin(answer).replace('0b','').count('1'):
+        if bin(n).count('1') == bin(answer).count('1'):
             return answer
         
         
